31be112c048eb986f3f7a7ca264e72ce3138cbbe
mohamedarafatelbadry <mohamed.arafat.elbadry@gmail.com>
2024-09-17 15:34:19 +0300

    factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Domains\Category\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'product_id' => $this->faker->word,
+        'title' => $this->faker->word,
+        'desc' => $this->faker->word,
+        'image' => $this->faker->word,
+        'active' => $this->faker->word,
+        'created_at' => $this->faker->date('Y-m-d H:i:s'),
+        'updated_at' => $this->faker->date('Y-m-d H:i:s')
+        ];
+    }
+}
